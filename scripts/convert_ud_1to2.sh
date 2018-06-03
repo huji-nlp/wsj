56f170ac9d274da17b1c138e6d265436c1fd5118
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-06-03 10:51:28 +0300

    Moved to UniversalConceptualCognitiveAnnotation/UCCA_English-WSJ

--- scripts/convert_ud_1to2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR=$(dirname $(readlink -f $0))
-
-for f in $DIR/../00/conllu{,.enhanced{,++}}/*; do
-  mv $f $f.tmp || exit 1
-  udapy -s ud.Convert1to2 < $f.tmp > $f || exit 1
-  rm -f $f.tmp || exit 1
-done
